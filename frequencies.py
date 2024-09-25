f2706fd48fb57edf19915f9ca3f707a3a3fa5f64
Lian Matsuo <lian.matsuo@kcl.ac.uk>
2024-09-25 11:53:19 +0100

    answer

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,14 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    # Your code goes here    
+    for item in items:
+        # If the item is already in the dictionary, increment its count
+        item = str(item)
+        if item in frequencies:
+            frequencies[item] += 1
+        # If the item is not in the dictionary, add it with count 1
+        else:
+            frequencies[item] = 1
+
     return frequencies
